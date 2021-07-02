839e34b1393bdc2f9f62322308f017266b80edc9
Brian Patino <patinobrian@gmail.com>
2021-07-02 17:38:29 -0400

    add styles to blog page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/Link'
+import Date from '../components/date'
 
 import { getSortedPostsData } from '../lib/posts'
 
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,8 @@
 import Layout from '../../components/layout'
+import Date from '../../components/date'
+import Head from 'next/head'
+
+import utilStyles from '../../styles/utils.module.css'
 
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
@@ -12,7 +16,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
       props: {
         postData
@@ -23,11 +27,17 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
     return (
         <Layout>
-          {postData.title}
+          <Head>
+            <title>{postData.title}</title>
+          </Head>
+          <article>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+              <Date dateString={postData.date} />
+            </div>
+          </article>
           <br />
-          {postData.id}
-          <br />
-          {postData.date}
+          <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
         </Layout>
       );
 }
